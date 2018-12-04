5ce368969c27befd9fd5ce0175af7845decccf09
Ammar <ammar.alnatsheh@gmail.com>
2018-12-04 00:08:20 -0800

    add the search bar and bind it to onsearch fun

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MovieList from '../components/MovieList.js';
+import Search from './Search.js';
 import exampleMovieData from '../data/exampleMovieData.js';
 
 class App extends React.Component {
@@ -11,11 +12,19 @@ class App extends React.Component {
     }
   }
 
+  onSearchClick(text) {
+    console.log(text);
+
+  }
+
   render() {
     return (
       <div>
         <div className = "header">
           <h3>Movie List</h3>
+        </div>
+        <div className="search-bar">
+          <Search searchClick={this.onSearchClick.bind(this)}/>
         </div>
         <div className="app-body">
           <MovieList movies={this.state.movies}/>
